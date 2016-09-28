8351c6aad5b2df7494359e99b01c4ce0dd6ac23f
salehvw0011 <salehfffaaa@gmail.com>
2016-09-28 14:23:58 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 import redis
 r = redis.Redis('localhost')
 from tempmail import TempMail
-token = 'TOKEN'
+token = '299258808:AAEP7OdjBe_qBQNhabAjIVaa_ICeXDWwynw'
 bot = telebot.TeleBot(token)
 user = bot.get_me().username
 @bot.message_handler(commands=['start'])
